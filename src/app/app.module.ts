81c8a766f40103871a6e6edeef3db68240c2f997
Mihir Modi <mihir.modi@smartsensesolutions.com>
2023-08-11 10:18:26 +0530

    FundFlex Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { environment } from 'src/environments/environment.prod';
+import { environment } from "src/environments/environment.prod";
 import { AngularFireModule } from "@angular/fire/compat";
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireAuth, AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+import {
+  AngularFireAuth,
+  AngularFireAuthModule,
+} from "@angular/fire/compat/auth";
 
-import { ClientService } from './services/client.service';
-import { AuthService } from './services/auth.service';
-import { SettingsService } from './services/settings.service';
+import { ClientService } from "./services/client.service";
+import { AuthService } from "./services/auth.service";
+import { SettingsService } from "./services/settings.service";
 
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ClientsComponent } from './components/clients/clients.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { AddClientComponent } from './components/add-client/add-client.component';
-import { EditClientComponent } from './components/edit-client/edit-client.component';
-import { ClientDetailsComponent } from './components/client-details/client-details.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { SettingsComponent } from './components/settings/settings.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { ClientsComponent } from "./components/clients/clients.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { AddClientComponent } from "./components/add-client/add-client.component";
+import { EditClientComponent } from "./components/edit-client/edit-client.component";
+import { ClientDetailsComponent } from "./components/client-details/client-details.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { SettingsComponent } from "./components/settings/settings.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
 
 @NgModule({
   declarations: [
@@ -39,16 +41,16 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
     LoginComponent,
     RegisterComponent,
     SettingsComponent,
-    NotFoundComponent
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase,'clientpanel'),
-    AngularFireAuthModule
+    AngularFireModule.initializeApp(environment.firebase, "fundflex"),
+    AngularFireAuthModule,
   ],
   providers: [ClientService, AuthService, SettingsService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   firebase: {
     apiKey: "AIzaSyAngel9Plnrf2sNLCCJMrCvkMwdfIbSegA",
-    authDomain: "clientpanelprod-ce909.firebaseapp.com",
-    projectId: "clientpanelprod-ce909",
-    storageBucket: "clientpanelprod-ce909.appspot.com",
+    authDomain: "fundflexprod-ce909.firebaseapp.com",
+    projectId: "fundflexprod-ce909",
+    storageBucket: "fundflexprod-ce909.appspot.com",
     messagingSenderId: "248313360011",
     appId: "1:248313360011:web:ed496a48bb2beff373ee8e",
-    measurementId: "G-Y1G5ZJ085H"
-  }
+    measurementId: "G-Y1G5ZJ085H",
+  },
 };
